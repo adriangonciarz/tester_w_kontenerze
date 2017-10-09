79716f88832b9753aa257f6636bfe21da0711603
Adrian Gonciarz <adrian.gonciarz@appdate.pl>
2017-10-09 12:30:07 +0200

    changes

--- test-code/src/main/java/models/Post.java
+++ test-code/src/main/java/models/Post.java
@@ -1,14 +1,42 @@
 package models;
 
-public class Post {
-    public int userId;
-    public String title;
-    public String body;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    public PostModel populateMinimum(){
-        this.userId=1;
-        this.title="New Title";
-        this.body = "This Body";
-        return this;
+public class Post {
+    @JsonProperty
+    private int userId;
+    @JsonProperty
+    private String title;
+    @JsonProperty
+    private String body;
+
+    public Post(int userId, String title, String body) {
+        this.setUserId(userId);
+        this.setTitle(title);
+        this.setBody(body);
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
     }
 }

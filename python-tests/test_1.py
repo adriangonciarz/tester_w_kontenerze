e6be72354011e24a04540275453f989782d0c1e8
Adrian Gonciarz <adrian.gonciarz@appdate.pl>
2018-09-14 07:13:21 +0200

    more updates

--- python-tests/test_1.py
+++ python-tests/test_1.py
@@ -1,10 +1,12 @@
 import os
-
+import logging
 import requests
 
+logging.basicConfig(level=logging.DEBUG)
 BASE_URI = os.environ.get("BASE_URI")
-
+log = logging.getLogger()
 
 def test_get_posts():
+    log.debug('Using BASE_URI: %s' % BASE_URI)
     r = requests.get('%s/posts' % BASE_URI)
     assert r.status_code == 200, "Expected code 200 but got: %s" % r.status_code

3e97dbb3876b9fdd99c6c026293504372b455a41
Adrian Gonciarz <adrian.gonciarz@appdate.pl>
2018-11-09 23:17:02 +0100

    Better tests boilerplate

--- python-tests/test_1.py
+++ python-tests/test_1.py
@@ -6,7 +6,18 @@ logging.basicConfig(level=logging.DEBUG)
 BASE_URI = os.environ.get("BASE_URI")
 log = logging.getLogger()
 
+
 def test_get_posts():
-    log.debug('Using BASE_URI: %s' % BASE_URI)
-    r = requests.get('%s/posts' % BASE_URI)
+    """Get /posts endpoint and assert 200 OK response"""
+
+    log.debug(f'Using Base URI: {BASE_URI}')
+    r = requests.get(f'{BASE_URI}/posts')
     assert r.status_code == 200, "Expected code 200 but got: %s" % r.status_code
+
+
+def test_assert_four_results():
+    """Get /posts endpoint and assert 4 items are present"""
+
+    posts = requests.get(f'{BASE_URI}/posts').json()
+    assert len(posts) == 4
+    assert 'provident' in posts[0]['title']
